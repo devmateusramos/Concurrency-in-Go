0c687b5039b320a641c079c68f2b375b1c8d6fb8
devmateusramos <devmateusramos@gmail.com>
2022-07-24 14:15:51 -0300

    second solution wait groups

--- 1-initial/main.go
+++ 1-initial/main.go
@@ -2,15 +2,61 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(phrase string) {
+func printSomething(phrase string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(phrase)
 }
 
 func main() {
-	go printSomething("This is the first thing to be printed!")
-	time.Sleep(time.Second)
-	printSomething("This is the second thing to be printed!")
+	var waitgroup sync.WaitGroup
+
+	waitgroup.Add(1)
+	go printSomething("This is the first thing to be printed!", &waitgroup)
+	waitgroup.Wait()
+
+	waitgroup.Add(1)
+	printSomething("This is the second thing to be printed!", &waitgroup)
 }
+
+// First solution and the worst
+
+//import (
+//"fmt"
+//"time"
+//)
+//
+//func printSomething(phrase string) {
+//	fmt.Println(phrase)
+//}
+//
+//func main() {
+//	go printSomething("This is the first thing to be printed!")
+//	time.Sleep(time.Second)
+//	printSomething("This is the second thing to be printed!")
+//}
+
+// Second solution wait groups
+
+//import (
+//"fmt"
+//"sync"
+//)
+//
+//func printSomething(phrase string, wg *sync.WaitGroup) {
+//	defer wg.Done()
+//	fmt.Println(phrase)
+//}
+//
+//func main() {
+//	var waitgroup sync.WaitGroup
+//
+//	waitgroup.Add(1)
+//	go printSomething("This is the first thing to be printed!", &waitgroup)
+//	waitgroup.Wait()
+//
+//	waitgroup.Add(1)
+//	printSomething("This is the second thing to be printed!", &waitgroup)
+//}
